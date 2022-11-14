d1576ed0ed95f6fd71a78f8ed999e357b0898c4b
Bartek Wesolowski <bartosz.wesolowski887@gmail.com>
2022-11-14 16:40:19 +0100

    Added login spinner, code refactor.

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
-## Useful commands
+## Serverless notes app
+
+Notes app implemented with serverless framework on AWS. Repo created for course [SST Guide](https://sst.dev/#preface) 
 
 ## First set up
-Run `npm install`
+1. Install node `brew install node` (version v19.0.1 tested)
+2. Install AWS CLI `brew install awscli`
+3. Configure `aws`:
+4. Run `npm install`
+
+## Useful commands
 
 ### Live lambda development
+
 Read more [here](https://docs.sst.dev/live-lambda-development)
 
 `npx sst start`
 
-
 ### Live local frontend development
 
 In `frontend` directory run `npm start`
+
+### Deploy stage to AWS
+
+`sst deploy --stage stage-name`
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
 import Routes from "./Routes";
 import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
-function App() {
+import { AppContext, useAppContext } from "./lib/contextLib";
+import { Auth } from "aws-amplify";
+import { useNavigate } from "react-router-dom";
+import { onError } from "./lib/errorLib";
+
+function NavigationBar() {
+  const { isAuthenticated, userHasAuthenticated } = useAppContext();
+  const nav = useNavigate();
+
+  async function logout(event) {
+    event.preventDefault();
+    await Auth.signOut();
+    userHasAuthenticated(false);
+    nav("/login");
+  }
+
   return (
-    <div className="App container py-3">
-      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <LinkContainer to="/">
-          <Navbar.Brand className="font-weight-bold text-muted">
-            Scratch
-          </Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav activeKey={window.location.pathname}>
-            <LinkContainer to="/signup">
-              <Nav.Link>Signup</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Routes />
-    </div>
+    <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+      <LinkContainer to="/">
+        <Navbar.Brand className="font-weight-bold text-muted">
+          Scratch
+        </Navbar.Brand>
+      </LinkContainer>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Nav activeKey={window.location.pathname}>
+          {isAuthenticated ? (
+            <Nav.Link onClick={logout}>Logout</Nav.Link>
+          ) : (
+            <>
+              <LinkContainer to="/signup">
+                <Nav.Link>Signup</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+            </>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    } catch (e) {
+      if (e !== "No current user") {
+        onError(e);
+      }
+    }
+    console.log("checking current session finished");
+    setIsAuthenticating(false);
+  }
+
+  return (
+    !isAuthenticating && (
+      <div className="App container py-3">
+        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+          <NavigationBar />
+          <Routes />
+        </AppContext.Provider>
+      </div>
+    )
   );
 }
 
